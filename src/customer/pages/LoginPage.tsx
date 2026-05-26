90011d00ce76647c669d3e5b4e225a2df99701ca
ssulochanaherath <ssulochanaherath@gmail.com>
2026-05-26 08:40:19 +0530

    set navigate

--- src/customer/pages/LoginPage.tsx
+++ src/customer/pages/LoginPage.tsx
@@ -30,7 +30,9 @@ export default function LoginPage() {
 
   return (
     <AuthCard>
-      <div className="flex items-center justify-between mb-10">
+      <div
+          onClick={() => navigate('/')}
+          className="flex items-center justify-between mb-10">
         <button className="text-black hover:opacity-70 transition-opacity">
           <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
