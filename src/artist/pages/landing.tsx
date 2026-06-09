567a2fbadaf59d068a8d65d31905bcfd758e9d56
ssulochanaherath <ssulochanaherath@gmail.com>
2026-06-09 13:36:21 +0530

    few changes

--- src/artist/pages/landing.tsx
+++ src/artist/pages/landing.tsx
@@ -587,6 +587,12 @@ export default function HomePage() {
             scroll-margin-top: 90px;
         }
 
+        #how-it-works,
+        #join-section,
+        #contact-section {
+            scroll-margin-top: 73px;
+        }
+
         /* Carousel Styles */
         .hide-scrollbar::-webkit-scrollbar {
             display: none;
